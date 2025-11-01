1a0e117782790ddb21a01d80c0995253be0be4f5
Anjali Kedia <24kedia.anjali@gmail.com>
2025-11-01 17:34:45 +0545

    feat: set up vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@
 import type { Metadata } from 'next';
 import { offbit, aeonik, lordStory, mightMakesRight, cooperLt, tourney, spicyRice } from './fonts';   //  ⟵ local-font imports
 import './globals.css';
+import { Analytics } from "@vercel/analytics/next"
 
 export const metadata: Metadata = {
   title: 'Yash | Portfolio',
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en" className={`${offbit.variable} ${aeonik.variable} ${lordStory.variable} ${mightMakesRight.variable} ${cooperLt.variable} ${tourney.variable} ${spicyRice.variable}`}>
       <body className="antialiased bg-[#FFFEF5]">
         {children}
+        <Analytics />
       </body>
     </html>
   );
